17c7755fa52b7d8c54a18cea17e8b1aba5329ac6
Shin-3117 <tlswnd3127@gmail.com>
2023-11-07 10:41:03 +0900

    통신 성공, 데이터 처리 성공

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,48 +1,59 @@
 <script setup>
 import { ref } from 'vue'
-import axios from 'axios'
+import getDepositList from './api/bank';
 const count = ref('rkskar')
 
-const baseURL = 'http://finlife.fss.or.kr'
-const key = '7ba015560fa92446dca297578a318783'
-const getList = async () => {
-  try{
-    const response = await axios.get(
-      `${baseURL}/finlifeapi/depositProductsSearch.json`, {
-      params: {
-        auth: key,
-        topFinGrpNo: '020000',
-        pageNo: 1
-      }
-    })
-    console.log(response.data)
-    count.value = response.data
-  } catch(error){
-    console.error(error)
-  }
-}
-getList()
-// axios.get('https://jsonplaceholder.typicode.com/todos/1')
-//   .then((response) => {
-//     // 성공 핸들링
-//     console.log(response);
-//     // count.value = response.data
-//     return response
-//   })
-//   .catch((error) => {
-//     // 에러 핸들링
-//     console.log(error);
-//   })
+const data = ref('')
 
+const getData = async ()=>{
+  data.value = await getDepositList()
+  // console.log(data.value)
+  const dataSet = (data)=>{
+      for (const base of data.value.result.baseList){
+      base.options = []
+      for (const option of data.value.result.optionList){
+        if (base.fin_prdt_cd === option.fin_prdt_cd){
+          base.options.push(option)
+        }
+      } 
+    }
+  }
+  await dataSet(data)
+}
 </script>
 
 <template>
   <div>
     {{ count }}
   </div>
+  <div v-if="data">
+    {{ data && data.result.total_count }}
+    <ul v-for="d in data.result.baseList" :key="d.fin_prdt_nm">
+      <span class="bankName">{{ d.kor_co_nm }}</span>
+      <span>{{ d.fin_prdt_nm }} </span>
+      <p>가입경로: {{ d.join_way }}</p>
+      <p>{{ d.mtrt_int }}</p>
+      <!-- <p>fin_co_no: {{ d.fin_co_no }}</p>
+      <p>fin_prdt_cd: {{ d.fin_prdt_cd }}</p> -->
+      <!-- <p>{{ d.options }}</p> -->
+      <p>---옵션---</p>
+      <li v-for="(option, index) in d.options" :key="index">
+        <p>기간: {{ option.save_trm }} 
+          <span>이자적용: {{ option.intr_rate_type_nm }}</span>
+          <span>이자율: {{ option.intr_rate }}</span>
+          <span>우대이자율: {{ option.intr_rate2 }}</span>
+        </p>
+      </li>
+      <hr>
+    </ul>
+  </div>
+  <button v-on:click="getData()">금융정보 부르기</button>
 </template>
 
 
 <style scoped>
-
+.bankName{
+  padding: 8px;
+  
+}
 </style>
